09affb56cb3e7785e8a1ef30ede690d228cef929
Brandon Buttlar <brandonbuttlar@mail.missouri.edu>
2019-02-15 23:54:56 -0600

    Finalized code

--- Document/Program.cs
+++ Document/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Code for INFOTC 2040's Document challenge (incomplete)
+// By Brandon Buttlar
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +8,50 @@ using System.Threading.Tasks;
 
 namespace Document
 {
+    public class Document
+    {
+        public string name, filename;
+
+        public void Init()
+        {
+            Console.WriteLine("Document\n\nDocument name: ");
+        }
+
+        public void Filename()
+        {
+            name = Console.ReadLine();
+            if (name == "0") // Used to test catch
+            {
+                throw new System.Exception();
+            }
+            filename = $"{name}.txt";
+        }
+
+        public void Exit()
+        {
+            Console.WriteLine($"{filename} was successfully saved. The document contains [count] characters.");
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Document\n\n");
+            Document d = new Document();
 
-            Console.ReadKey();
+            d.Init();
+            try
+            {
+                d.Filename();
+                d.Exit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}\n{ex.StackTrace}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
